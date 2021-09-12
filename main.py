133c8e23045c206786454d7ae7df77dc1398d3c1
plubpe <78285973+plubpe@users.noreply.github.com>
2021-09-12 10:00:36 +0700

    Update main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -6,30 +6,77 @@ tiles.place_on_random_tile(Sprites,assets.tile("""block"""))
 tiles.place_on_random_tile(Ai,assets.tile("""black"""))
 Ai.say("Calculate 1 number from 0 to 100.")
 pause(5000)
-x = 50
+a = 100
+b = 0
+c = 1
+x = randint(b, a+1)
 Ai.say(x)
-a = 49
-d = 0
-c = 0
-b = 1
-g = 0
 
 def on_event_pressed():
-    global b,a,x,c,d
-    if b==1:
-        x = randint(0, 49)
-        Ai.say(x)
-        a=x
-
+    global a,b,c,x
+    a=x-1
+    x = randint(b, a+1)
+    Ai.say(x)
+    c=c+1
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_event_pressed)
 
 def on_event_pressed1():
-    global b,a,x,c,d,g
-    if b==1:
-        x = randint(51, 100)
-        Ai.say(x)
-        c=x
+    global a,b,c,x
+    b=x+1
+    x = randint(b, a+1)
+    Ai.say(x)
+    c=c+1
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_event_pressed1)
+
+def on_event_pressed2():
+    global c
+    Ai.say(c)
+    pause(1000)
+    game.over(True)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_event_pressed2)
+
+#def on_event_pressed():
+    #global b,a,x,c,d
+    #if b==1:
+        #x = randint(0, 49)
+        #Ai.say(x)
+        #a=x
+    #elif x < 49:
+        #x = randint(c, a)
+        #Ai.say(x)
+        #c=x
+    #elif c > 49:
+        #x = randint(c, a)
+        #Ai.say(x)
+        #c=x
+    #elif a == 25:
+        #x = randint(c, a)
+        #Ai.say(x)
+        #c=x
+    #b=b+1
+#controller.up.on_event(ControllerButtonEvent.PRESSED, on_event_pressed)
+
+#def on_event_pressed1():
+    #global b,a,x,c,d
+    #if b==1:
+        #x = randint(51, 100)
+        #Ai.say(x)
+        #c=x
+        #a=100
+    #elif x < 100:
+        #x = randint(c, a) 
+        #Ai.say(x)
+        #c=x
+    #elif a < 49:
+        #x = randint(c, a)
+        #Ai.say(x)
+        #a=x
+    #elif c == 75:
+        #x = randint(c, a)
+        #Ai.say(x)
+        #a=x
+    #b=b+1
+#controller.down.on_event(ControllerButtonEvent.PRESSED, on_event_pressed1)
 
 #def on_event_pressed():
     #global b,a,x,c,d
@@ -65,9 +112,3 @@ controller.down.on_event(ControllerButtonEvent.PRESSED, on_event_pressed1)
         #Ai.say(x)
     #b = b+1
 #controller.down.on_event(ControllerButtonEvent.PRESSED, on_event_pressed1)
-
-def on_event_pressed2():
-    Ai.say(b)
-    pause(500)
-    game.over(True)
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_event_pressed2)
--- main.ts
+++ main.ts
@@ -6,66 +6,28 @@ tiles.placeOnRandomTile(Sprites, assets.tile`block`)
 tiles.placeOnRandomTile(Ai, assets.tile`black`)
 Ai.say("Calculate 1 number from 0 to 100.")
 pause(5000)
-let x = 50
+let a = 100
+let b = 0
+let c = 1
+let x = randint(b, a + 1)
 Ai.say(x)
-let a = 49
-let d = 0
-let c = 0
-let b = 1
-let g = 0
 controller.up.onEvent(ControllerButtonEvent.Pressed, function on_event_pressed() {
     
-    if (b == 1) {
-        x = randint(0, 49)
-        Ai.say(x)
-        a = x
-    }
-    
+    a = x - 1
+    x = randint(b, a + 1)
+    Ai.say(x)
+    c = c + 1
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function on_event_pressed1() {
     
-    if (b == 1) {
-        x = randint(51, 100)
-        Ai.say(x)
-        c = x
-    }
-    
+    b = x + 1
+    x = randint(b, a + 1)
+    Ai.say(x)
+    c = c + 1
 })
-// def on_event_pressed():
-// global b,a,x,c,d
-// if b==1:
-// x = randint(0, a)  
-// Ai.say(x)
-// a=x
-// else :
-// x = randint(c,d)
-// Ai.say(x)
-// d=c
-// Sprites.say(d)
-// a=x
-// if x > 100:
-// x = 100
-// Ai.say(x)
-// b = b+1
-// controller.up.on_event(ControllerButtonEvent.PRESSED, on_event_pressed)
-// def on_event_pressed1():
-// global b,a,x,c,d,g
-// if b==1:
-// c=51
-// x = randint(c, 100)
-// Ai.say(x)
-// elif x < d:
-// g = randint(1, 10)
-// x = x+g
-// Ai.say(x)
-// c=x
-// elif x > 100:
-// x = 100
-// Ai.say(x)
-// b = b+1
-// controller.down.on_event(ControllerButtonEvent.PRESSED, on_event_pressed1)
 controller.A.onEvent(ControllerButtonEvent.Pressed, function on_event_pressed2() {
-    Ai.say(b)
-    pause(500)
+    
+    Ai.say(c)
+    pause(1000)
     game.over(true)
 })
